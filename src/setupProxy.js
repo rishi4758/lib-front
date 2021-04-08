f0e019a61b2c2a219a7d44151ddd64b93d1b4512
Rishav Rapta <rishavrapta@Rishavs-Air.lan>
2021-04-08 23:39:35 +0300

    lib-front

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,1 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware({
-      target: "http://libback-env.eba-gpizad5f.us-east-2.elasticbeanstalk.com",
-      changeOrigin: true,
-    })
-  );
-};
